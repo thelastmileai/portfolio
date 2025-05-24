763b2200a185a490953d2669070012f7692a1ebe
thelastmileai <nooraa7045@gmail.com>
2025-05-24 22:49:09 +0530

    Update About.tsx

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -24,7 +24,7 @@ const About: React.FC = () => {
             style={{ maxHeight: '500px' }}
           >
            <img
-  src="https://static.vecteezy.com/system/resources/previews/025/794/537/non_2x/asian-woman-in-hijab-working-on-laptop-beautiful-woman-in-hijab-smiling-using-laptop-illustration-free-download-free-vector.jpg"
+  src="https://drive.google.com/file/d/1tz2_Upyl-6l4OUKJUsaiGjgpO21ozjDY/view?usp=sharing"
   alt="Hijabi Woman"
   className="w-full h-auto object-cover"
 />
