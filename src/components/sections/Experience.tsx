d43a493c88031565ca5abdc645f0c19d7e11fa67
thelastmileai <nooraa7045@gmail.com>
2025-05-24 19:31:44 +0530

    Update Experience.tsx

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -190,7 +190,9 @@ const Experience: React.FC = () => {
                   
                   {job.expanded && (
   <div className={`mt-4 space-y-2 animate-fadeIn ${index % 2 === 0 ? 'text-left' : 'text-right'}`}>
-    <h4 className="font-medium">Key Responsibilities:</h4>
+    <h4 className={`font-medium ${index % 2 === 0 ? 'text-left md:text-left' : 'text-right md:text-right'}`}>
+  Key Responsibilities:
+</h4>
     <ul className="list-disc ml-5 space-y-1">
       {job.responsibilities.map((responsibility, i) => (
         <li key={i}>{responsibility}</li>
